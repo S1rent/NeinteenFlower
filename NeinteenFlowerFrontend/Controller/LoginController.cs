acb776825fb4e56c3163dd1cb779aab3bf1c21dd
Philip Indra Prayitno <philip.prayitno@gmail.com>
2021-06-02 18:12:54 +0700

    fix: web service error

---
+++ NeinteenFlowerFrontend/Controller/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using NeinteenFlower_FrontEnd.Handler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeinteenFlower_FrontEnd.Controller
+{
+    public class LoginController
+    {
+        CommonHandlerFrontEnd handler = new CommonHandlerFrontEnd();
+        public LoginController() { }
+        public string Login(string email, string password)
+        {
+            bool isMember = handler.CheckMemberEmailExist(email);
+            bool isEmployee = handler.CheckEmployeeEmailExist(email);
+
+            if (email.Length == 0)
+            {
+                return "Email cannot be empty.";
+            }
+            else if (password.Length == 0)
+            {
+                return "Password cannot be empty.";
+            }
+            else if (!isMember && !isEmployee)
+            {
+                return "Email is not found.";
+            }
+            else if (isMember)
+            {
+                bool isPasswordValid = handler.ValidatePassword(false, email, password);
+                if (isPasswordValid)
+                {
+                    return "";
+                }
+                else
+                {
+                    return "Wrong password.";
+                }
+            }
+            else if (isEmployee)
+            {
+                bool isPasswordValid = handler.ValidatePassword(true, email, password);
+                if (isPasswordValid)
+                {
+                    return "";
+                }
+                else
+                {
+                    return "Wrong password.";
+                }
+            }
+            else
+            {
+                return "Error, Please try again later.";
+            }
+        }
+    }
+}
---
+++ NeinteenFlowerFrontend/Handler/CommonHandlerFrontEnd.cs
@@ -0,0 +1,33 @@
+﻿using NeinteenFlower.Handler;
+using NeinteenFlower.WebService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeinteenFlower_FrontEnd.Handler
+{
+    public class CommonHandlerFrontEnd
+    {
+        public NeinteenFlowerWebService webService = new NeinteenFlowerWebService();
+        public CommonHandlerFrontEnd() { }
+
+        public bool CheckMemberEmailExist(string email)
+        {
+            string isUserEmailExist = webService.CheckMemberEmailExist(email);
+            return JSONHandler.shared.Decode<bool>(isUserEmailExist);
+        }
+
+        public bool CheckEmployeeEmailExist(string email)
+        {
+            string isUserEmailExist = webService.CheckEmployeeEmailExist(email);
+            return JSONHandler.shared.Decode<bool>(isUserEmailExist);
+        }
+
+        public bool ValidatePassword(bool isEmployee, string email, string password)
+        {
+            string isPasswordValid = webService.ValidatePassword(isEmployee, email, password);
+            return JSONHandler.shared.Decode<bool>(isPasswordValid);
+        }
+    }
+}
